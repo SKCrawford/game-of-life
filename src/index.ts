7052763dc2c369d84d990741b4809f6a96ff60c1
SKCrawford <s.kyle.crawford@gmail.com>
2019-08-12 16:52:58 -0700

    Add simplistic logging of ticks and grids. - Create basic logger - Add command for starting the server with file-based logging. - Add logs to track ticks and their grid evolutions.

--- src/index.ts
+++ src/index.ts
@@ -1,25 +1,33 @@
 import { createServer } from './server';
 import { EvolutionService } from './evolve';
+import { Logger } from './logger';
+
+const logger = new Logger(main.name);
 
 async function main() {
   const io = createServer();
   const port = 8000;
   io.listen(port);
+  logger.info(`Started server on port ${port}`);
 
   io.on('connection', socket => {
     const id = socket.id;
     socket.emit('init', id);
 
-    socket.on(id, (dto, fn) => {
+    socket.on(id, dto => {
+      let tick = 0;
       const service = new EvolutionService();
-      console.log(id, dto);
 
       const evolutions = service.evolve(dto.seed);
       setInterval(() => {
+        tick++;
         const grid = evolutions.next().value;
-        socket.emit(id, grid);
+        logger.info(`Tick: ${tick}`);
+        logger.info('Grid:');
+        logger.grid(grid);
+        socket.emit(id, { tick, grid });
       }, dto.delay)
     });
   });
 }
-main().catch(console.error);
+main().catch(logger.error);
---
+++ src/logger.ts
@@ -0,0 +1,59 @@
+import chalk from 'chalk';
+
+enum LogLevels {
+  Error = 0,
+  Warn = 1,
+  Info = 2,
+  Debug = 3,
+}
+
+export class Logger {
+  private context: string;
+  private level: number;
+
+  constructor(context: string, level: LogLevels = LogLevels.Debug) {
+    this.context = context;
+    this.level = level;
+  }
+
+  public get prefix() {
+    const timestamp = new Date().toISOString();
+    return `[${timestamp}][${this.context}]`;
+  }
+
+  public debug(...args: any[]) {
+    if (this.level >= LogLevels.Debug) {
+      const msg = chalk.blue('DBG', this.prefix, ...args);
+      console.debug(msg);
+    }
+  }
+
+  public info(...args: any[]) {
+    if (this.level >= LogLevels.Info) {
+      const msg = chalk.green('INF', this.prefix, ...args)
+      console.info(msg);
+    }
+  }
+
+  public warn(...args: any[]) {
+    if (this.level >= LogLevels.Warn) {
+      const msg = chalk.yellow('WRN', this.prefix, ...args)
+      console.warn();
+    }
+  }
+
+  public error(...args: any[]) {
+    if (this.level >= LogLevels.Error) {
+      const msg = chalk.red('ERR', this.prefix, ...args)
+      console.error(msg);
+    }
+  }
+
+  public grid<T>(grid: T[][]) {
+    const toPrettyRow = (cell: T) => Boolean(cell) ? 'X' : 'O';
+    grid.forEach(row => {
+      const prettyrow = row.map(toPrettyRow);
+      console.info(chalk.green(...prettyrow))
+    })
+  }
+}
