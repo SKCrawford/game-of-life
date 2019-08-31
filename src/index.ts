103e01a8b990fc37633fe98a9bd6fd6fdfdcb980
SKCrawford <s.kyle.crawford@gmail.com>
2019-08-31 16:10:29 -0700

    Add clean-up and improve logging

--- src/index.ts
+++ src/index.ts
@@ -11,23 +11,37 @@ async function main() {
   logger.info(`Started server on port ${port}`);
 
   io.on('connection', socket => {
-    const id = socket.id;
-    socket.emit('init', id);
+    logger.debug('Connected');
+    const service = new EvolutionService(); // One client, one service
+    let timerId: NodeJS.Timeout;
 
-    socket.on(id, dto => {
+    // Send the unique ID to the client so that grids are not sent to each client
+    socket.emit('init', socket.id);
+
+    // When a client emits their unique ID, a seed grid, and a delay, 
+    // send the grid evolutions
+    socket.on(socket.id, dto => {
+      // Get an iterator of the grid's evolutions
       let tick = 0;
-      const service = new EvolutionService();
-
       const evolutions = service.evolve(dto.seed);
-      setInterval(() => {
+
+      // Emit each tick and grid evolution according to the provided delay
+      timerId = setInterval(() => {
         tick++;
         const grid = evolutions.next().value;
+        logger.info(`ID: ${socket.id}`);
         logger.info(`Tick: ${tick}`);
         logger.info('Grid:');
         logger.grid(grid);
-        socket.emit(id, { tick, grid });
+        socket.emit(socket.id, { tick, grid });
       }, dto.delay)
     });
+
+    // Clean up by stopping the loop when the client disconnects
+    socket.on('disconnect', () => {
+      clearInterval(timerId);
+    })
   });
 }
+
 main().catch(logger.error);
