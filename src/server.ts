fb53060c299ae96a0a0ecd0436453e81803a23be
SKCrawford <s.kyle.crawford@gmail.com>
2020-02-12 20:32:28 -0800

    Add cors.

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,11 @@
+import * as cors from 'cors';
 import * as Http from 'http';
 import * as Io from 'socket.io';
 import * as Express from 'express';
 
 export function createServer() {
   const app = Express();
+  app.use(cors());
   const server = Http.createServer(app);
   return Io(server);
 }
